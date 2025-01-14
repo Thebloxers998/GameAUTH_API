89f3482156e9ea781cb839c9f46e62c025ced08a
GoodBoi19 <181642507+Thebloxers998@users.noreply.github.com>
2025-01-14 17:50:25 +0000

    Create authControllers.js

---
+++ controllers/authControllers.js
@@ -0,0 +1,34 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const { sendEmail } = require('../utils/email');
+
+dotenv.config();
+
+const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    sendEmail(email, 'Welcome!', 'Thank you for registering!');
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { register, login };
